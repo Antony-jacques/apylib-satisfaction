86672c56ff0b373eed72456e0bdb9b62ba869c2f
Antony <antony.jacques@laposte.net>
2021-05-03 14:50:03 +0200

    hashing

--- app-installation/server/index.js
+++ app-installation/server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
+//hashing des MP
+const bcrypt = require('bcrypt')
+const saltRounds =10
+
 const app = express();
 
 app.use(express.json());
@@ -24,14 +28,55 @@ app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
+
+
+    bcrypt.hash(password, saltRounds, (err, hash)=>{
+
+      if(err){
+        console.log(err)
+      }
+
+      db.query(
+        "INSERT INTO users (username, password) VALUES (?,?)",
+        [username, hash],
+        (err, result) => {
+          console.log(err);
+        }
+      );
+
+    })
+
+
+});
+
+app.post('/login', (req, res)=>{
+  const username = req.body.username;
+    const password = req.body.password;
+
   db.query(
-    "INSERT INTO users (username, password) VALUES (?,?)",
-    [username, password],
+    "SELECT * FROM users WHERE username = ? ; ",
+    username,
     (err, result) => {
+
+      if(err){
+        res.send({err:err});
+      }
+
+      if(result.length > 0){
+        bcrypt.compare(password, result[0].password, (error, response)=>{
+          if(response){
+            res.send(result)
+          } else{
+            res.send({message:'Erreur d\'identifiant ou de mot de passe'});
+          }
+        })
+      } else{
+        res.send({message:'Cet utilisateur n\'existe pas'})
+      }
       console.log(err);
     }
   );
-});
+})
 
 app.listen(3001, () => {
   console.log("run 3001");
