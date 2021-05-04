70b8c6f4aa27bd3e0811a3b536ca12595b9788d3
Antony <antony.jacques@laposte.net>
2021-05-04 11:32:33 +0200

    session ok

--- app-installation/server/index.js
+++ app-installation/server/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+
+
 //hashing des MP
 const bcrypt = require('bcrypt')
 const saltRounds =10
@@ -9,7 +15,28 @@ const saltRounds =10
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:3000"],
+  methods : ["GET", "POST"],
+  credentials:true // autorise cookie
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended : true}));
+
+//initialiser la session
+app.use(session({
+  key : "userID", // nom du cookie
+  secret: "subscribe",
+  resave : false, 
+  saveUninitialized:false,
+  cookie : {
+     expires: 1000 * 60 * 60 * 24, // le cookie dure 1000 milisecondes x 60 (sec) x 60 (min) x 24 (heures) soit 1 jour
+
+  },
+}
+ 
+));
 
 
 const db = mysql.createConnection({
@@ -49,6 +76,16 @@ app.post('/register', (req, res) => {
 
 });
 
+app.get('/login', (req, res)=>{
+  if(req.session.user){
+    res.send({loggedIn: true, user: req.session.user})
+  } else{
+    res.send({loggedIn: false })
+  }
+})
+
+app.get
+
 app.post('/login', (req, res)=>{
   const username = req.body.username;
     const password = req.body.password;
@@ -65,6 +102,8 @@ app.post('/login', (req, res)=>{
       if(result.length > 0){
         bcrypt.compare(password, result[0].password, (error, response)=>{
           if(response){
+            req.session.user = result;
+            console.log(req.session.user)
             res.send(result)
           } else{
             res.send({message:'Erreur d\'identifiant ou de mot de passe'});
